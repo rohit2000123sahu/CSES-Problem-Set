ac4bf54981e5d88e53c5ed01dcfb926855d9cdce
rohit2000123sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-02-13 14:24:54 +0530

    Create Subarray Divisibility.cpp

---
+++ Sorting and Searching/Subarray Divisibility.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(),x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector< ll > vl;
+typedef pair< ll , ll > pl;
+typedef unordered_map< ll, ll > uml;
+typedef vector< pl > vpl;
+typedef map< ll, ll > ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ll n,sum;
+    cin>>n;
+
+    ll a[n];
+    ll i;
+
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    ml umap;
+    sum=0;
+    ll ans=0;
+
+    for(i=0;i<n;i++){
+        sum+=a[i];
+        sum=((sum%n)+n)%n;
+        if(sum==0){
+            ans++;
+        }
+        ans+=umap[sum];     // if remainder are same then we can only get the sub array divisible by n
+        umap[sum]++;
+    }
+
+    cout<<ans<<endl;
+
+    return 0;
+}
