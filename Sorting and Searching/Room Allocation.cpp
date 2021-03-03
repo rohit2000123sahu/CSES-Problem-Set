998431c879e75d2b0271c749c30c53b9ae903962
rohit2000123sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-03-03 12:27:42 +0530

    Added the comments

--- Sorting and Searching/Room Allocation.cpp
+++ Sorting and Searching/Room Allocation.cpp
@@ -24,15 +24,15 @@ int main()
     ll n;
     cin>>n;
  
-    vpl arr(n),dep(n);
+    vpl arr(n),dep(n);    // Pair for storing the values as well as index
  
     ll i,j;
     for(i=0;i<n;i++){
         cin>>arr[i].first>>dep[i].first;
-        arr[i].second=dep[i].second=i;
+        arr[i].second=dep[i].second=i;   // Storing the index 
     }
  
-    sort(arr.begin(),arr.end());
+    sort(arr.begin(),arr.end());         // Sort them
     sort(dep.begin(),dep.end());
 
 
@@ -41,24 +41,24 @@ int main()
  
     ll ans=0;
     ll count=0;
-    ll rooms[n];
+    ll rooms[n];      // For storing the rooms allocated to the customers
 
     memset(rooms,0,sizeof(rooms));
-    priority_queue < ll ,vector< ll > , greater< ll > > vacant;
+    priority_queue < ll ,vector< ll > , greater< ll > > vacant;  // This will contains the rooms that are vacant 
  
     while(i<n && j<n){
-        if(arr[i].F<=dep[j].F){
+        if(arr[i].F<=dep[j].F){     
             count++;
-            if(vacant.size()==0){
+            if(vacant.size()==0){         // If no rooms are vacant then we need to give a new room to the customer
                 rooms[arr[i].S]=count;
             }else{
-                rooms[arr[i].S]=vacant.top();
+                rooms[arr[i].S]=vacant.top();  // If some rooms are vacant already then we can give these rooms to the customer
                 vacant.pop();
             }
             i++;
         }else{
             count--;
-            vacant.push(rooms[dep[j].S]);
+            vacant.push(rooms[dep[j].S]);  // If some customer leaves the room then push the room in to the vacant list
             j++;
         }
         ans=max(ans,count);
